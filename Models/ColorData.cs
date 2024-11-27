27edcfd5b848a0aeda7388cf74f570f861ac552e
MrInmortal434 <118313004+MrInmortal@users.noreply.github.com>
2024-11-27 13:30:38 -0400

    Add files via upload

---
+++ Models/ColorData.cs
@@ -0,0 +1,9 @@
+﻿namespace Tarea10.Models
+{
+    public class ColorData
+    {
+        public int Reds { get; set; }
+        public int Greens { get; set; }
+        public int Blues { get; set; }
+    }
+}
---
+++ Services/ColorService.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Tarea10.Models;
+
+namespace Tarea10.Services
+{
+    public class ColorService
+    {
+        private readonly HttpClient _http;
+
+        public ColorService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task<ColorData> GetColorDataAsync()
+        {
+            return await _http.GetFromJsonAsync<ColorData>("api/colorin") ?? new ColorData();
+        }
+
+        public async Task SaveColorDataAsync(ColorData data)
+        {
+            await _http.PostAsJsonAsync("api/colorin", data);
+        }
+    }
+}
